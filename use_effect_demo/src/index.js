31098b8e95c417a7d3f840f09a27cd2536f1d252
Fahim-at-work <shariff.fahim7@gmail.com>
2020-07-23 13:23:56 -0400

    added image, name, username, github url

--- use_effect_demo/src/index.js
+++ use_effect_demo/src/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
 function GitHubUser({login}){
@@ -12,7 +13,22 @@ useEffect(() => {
 }, []);
 
 if(data){
-return <div>{JSON.stringify(data)}</div>
+return (
+  <div className="container">
+    <table className="center">
+      <tr>
+        <td>
+        <img src={data.avatar_url} width='350'/>
+        </td>
+        <td>
+        <h1>Name: {data.name}</h1>
+        <h3>UserName: {data.login}</h3>
+        <a href={data.html_url}> GitHub Profile </a>
+        </td>
+      </tr>
+    </table>
+  </div>
+);
 }
 return null;
 }
