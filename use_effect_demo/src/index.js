af399971f83b83bc40d6dc05a5e484c7faafb1fe
Fahim-at-work <shariff.fahim7@gmail.com>
2020-07-23 12:55:51 -0400

    use of euseEffect fetching github user api

--- use_effect_demo/src/index.js
+++ use_effect_demo/src/index.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function GitHubUser({login}){
+const [data, setData] = useState(null);
+useEffect(() => {
+  fetch(`https://api.github.com/users/${login}`)
+  .then(res => res.json())
+  .then(setData)
+  .catch(console.error);
+}, []);
+
+if(data){
+return <div>{JSON.stringify(data)}</div>
+}
+return null;
+}
+
+function App(){
+return <GitHubUser login="Fahim-at-work" />
+
+}
+
+
 
 ReactDOM.render( 
-  <h1> Hello </h1>,
+  <App />,
     document.getElementById('root')
 );
